77d2df6a68c64f7c78a80ab26c189c6d96295be1
Maksim Orlov <orelmax@gmail.com>
2017-09-13 23:37:52 +0700

    Move App and Map to containers

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import GeoMap from '../Map';
+import Panel from '../Panel';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Panel />
         <GeoMap />
       </div>
     );
